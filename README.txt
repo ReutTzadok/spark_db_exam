74b29ca14ec1b2a12b3be72c0bffe30ee4575e20
Reut Tzadok <reut.b1212@gmail.com>
2021-07-01 16:53:19 +0300

    adding README.txt

---
+++ README.txt
@@ -0,0 +1,10 @@
+Some example for running:
+
+* find suspicious activities:
+    http://localhost:8080/api/suspicious?startTime=2020-02-22%2016:50:39&endTime=2020-08-22%2016:50:39
+
+* see the statistic for specific game run:
+    http://localhost:8080/api/game-statistics?startTime=2010-02-22%2016:50:39&endTime=2050-04-22%2016:50:39&game=pocker
+
+* see the statistics for all games, run:
+    http://localhost:8080/api/game-statistics?startTime=2010-02-22%2016:50:39&endTime=2050-04-22%2016:50:39&game
--- src/main/java/com/bets_processor/controllers/StatisticsController.java
+++ src/main/java/com/bets_processor/controllers/StatisticsController.java
@@ -5,6 +5,7 @@ import com.bets_processor.services.suspicious.SuspiciousActivities;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +21,13 @@ public class StatisticsController {
     private SuspiciousActivities suspiciousActivities;
 
 
-    //example for running: http://localhost:8080/api/game-statistics?startTime=2010-02-22%2016:50:39&endTime=2050-04-22%2016:50:39&game=pocker
-    //or: http://localhost:8080/api/game-statistics?startTime=2010-02-22%2016:50:39&endTime=2050-04-22%2016:50:39&game
+
     @GetMapping("suspicious")
     public String findSuspiciousActivities(Timestamp startTime, Timestamp endTime) {
         return suspiciousActivities.apply(startTime, endTime);
     }
 
-    //example for running: http://localhost:8080/api/suspicious?startTime=2020-02-22%2016:50:39&endTime=2020-08-22%2016:50:39
+
     @GetMapping(value = "game-statistics", params = "game")
     public String getGameStatistics(Timestamp startTime, Timestamp endTime, String game) {
         return gameStatistics.apply(startTime, endTime, game);
